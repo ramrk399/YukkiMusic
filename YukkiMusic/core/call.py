10db5d313239ff8a41c7c6c75773bc7bee5f04a6
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-09-26 08:06:34 +0530

    using force_stop_stream

--- YukkiMusic/core/call.py
+++ YukkiMusic/core/call.py
@@ -570,7 +570,8 @@ class Call(PyTgCalls):
         @self.four.on_update(filters.chat_update(ChatUpdate.Status.LEFT_CALL))
         @self.five.on_update(filters.chat_update(ChatUpdate.Status.LEFT_CALL))
         async def stream_services_handler(_, chat_id: int):
-            await self.stop_stream(chat_id)
+            await self.force_stop_stream(chat_id)
+
 
         @self.one.on_update(filters.stream_end)
         @self.two.on_update(filters.stream_end)
